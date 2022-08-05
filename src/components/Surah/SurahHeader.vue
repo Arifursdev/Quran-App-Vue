0f7e899ffe85eb8c789facafd38d2fb659769635
ArifursDev <52378239+Arifursdev@users.noreply.github.com>
2022-08-05 17:42:48 +0600

    backToChapters Button, Input fix for mobile

--- src/components/Surah/SurahHeader.vue
+++ src/components/Surah/SurahHeader.vue
@@ -5,6 +5,11 @@
         </template>
 
         <template v-if="surahInfo !== null">
+        
+            <div class="back-to-chapters" @click="goBackToChapters">
+                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="m8.5 12.8l5.7 5.6c.4.4 1 .4 1.4 0c.4-.4.4-1 0-1.4l-4.9-5l4.9-5c.4-.4.4-1 0-1.4c-.2-.2-.4-.3-.7-.3c-.3 0-.5.1-.7.3l-5.7 5.6c-.4.5-.4 1.1 0 1.6c0-.1 0-.1 0 0z"/></svg>
+            </div>
+
             <h2>{{ surahInfo.name_simple }}</h2>
             <p>{{ surahInfo.translated_name.name }}</p>
 
@@ -24,6 +29,11 @@ export default {
     name: "Header",
     components: {
         Loading,
+    },
+    methods: {
+        goBackToChapters(){
+            this.$store.commit('setCurrentSurah', null)
+        }
     },
     computed: {
         surahInfo(){
